ae12edaf1c0f8e97265f4be0d339edbc0727799a
rossannaryprak <rossannaryprak@gmail.com>
2025-06-14 14:03:40 +0700

    server.js

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Nary and Node.js app!');
-});
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`App is listening on port ${port}`);
-});
-=======
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello from your Node.js app running in Docker!');
-});
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello from Nary and Node.js app!');
-});
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`App is listening on port ${port}`);
-});
-=======
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -39,7 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello from your Node.js app running in Docker!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`App is listening on port ${PORT}`);
 });
->>>>>>> 0b3797f (Add express server)
