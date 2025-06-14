63b8ed999ee5b9d0b1c7130ac28bbe1ea7540575
rossannaryprak <rossannaryprak@gmail.com>
2025-06-14 11:54:22 +0700

    Update server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Nary and Node.js app!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App is listening on port ${port}`);
+});
